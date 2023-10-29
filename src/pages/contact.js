9a996bd446f99a4aa038e9d541c2301eab393a39
Mariana Ribas <mariana.rj89@gmail.com>
2023-10-29 01:08:08 -0300

    about, pokemons and contact placeholer pages

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/layout";
+
+const Contact = () => {
+    return (
+        <Layout>
+            <h1>Envie uma mensagem!</h1>
+        </Layout>
+    )
+}
+
+export default  Contact
+export const Head = () => <title>Fale conosco</title>
---
+++ src/pages/pokemons.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/layout";
+
+const Pokemons = () => {
+    return (
+        <Layout>
+            <h1>Os Pokemons</h1>
+        </Layout>
+    )
+}
+
+export default  Pokemons
+export const Head = () => <title>Os Pokemons</title>
